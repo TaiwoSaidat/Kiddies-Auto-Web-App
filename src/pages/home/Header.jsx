873e4800c91afae0c7d9edcb00423281bc1e7e56
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2024-10-28 18:08:38 +0100

    chore: fix ui for header + topContactHeader

--- src/pages/home/Header.jsx
+++ src/pages/home/Header.jsx
@@ -1,67 +1,55 @@
-import React from 'react'
-import applogo from '../../assets/applogo.png'
-import arrowDown from '../../assets/arrowdownblack.png'
-import search from '../../assets/searchblack.png'
-import account from '../../assets/profileblack.png'
-import cart from '../../assets/cartblack.png'
-import menu from  '../../assets/menu.png'
-import { Link } from 'react-router-dom'
-
-
+import React from "react";
+import applogo from "../../assets/applogo.png";
+import arrowDown from "../../assets/arrowdownblack.png";
+import search from "../../assets/searchblack.png";
+import account from "../../assets/profileblack.png";
+import cart from "../../assets/cartblack.png";
+import menu from "../../assets/menu.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
-    {/* bg-[#fcfcfc] */}
-    <nav className=' header w-full h-28 flex items-center justify-between  py-8 bg-[#fcfcfc] ' >
-        <div className="w-[90%] h-12 flex justify-between items-center mx-auto  flex-none order-none self-stretch grow-0 p-0;">
-            
-                <img src={applogo} alt="" /> 
+      {/* bg-[#fcfcfc] */}
+      {/* <nav className=' w-full h-28 flex items-center justify-between py-8 bg-[#fcfcfc] ' >
+        <div className="w-[90%] h-12 flex justify-between items-center mx-auto flex-none order-none self-stretch grow-0 p-0;"> */}
+      <div className="w-full  bg-[#fcfcfc]  ">
+        <div className=" flex items-center justify-between  mx-5">
+          <div className="">
+            <img src={applogo} alt="" />
+          </div>
+          <div className="hidden lg:flex  gap-10 ">
+            <div className="flex ">
+              <p>Categories</p>
+              <img src={arrowDown} alt="" />
+            </div>
+            <p>Deals</p>
+            <p>Whats New</p>
+            <p>Delivery</p>
+          </div>
 
-                <div className="hidden lg:flex flex-row items-end gap-10 w-[414px] h-6 flex-none order-1 grow-0 mx-auto my-0 p-0 ">
-                    <div className="flex items-start gap-0.5 w-[103px] h-6 flex-none order-none grow-0 p-0">
-                        <p>categories</p>
-                        <img src={arrowDown} alt="" />
-                    </div>
-                    <p>Deals</p>
-                    <p>Whats New</p>
-                    <p>Delivery</p>
-                </div>
-
-                <div className=' flex flex-row items-center gap-10 w-[543px] h-12 flex-none order-2 grow-0 mx-auto my-0 p-0 '>
-                <form action="">
-                    <label className="hidden input input-bordered rounded-3xl lg:flex items-center gap-2 w-[310px] h-12 bg-[f6f7f8] ">
-                        <input
-                            type="text"
-                            className=" pl-8"
-                            placeholder="Search"
-                        />
-                        <img className="w-4 h-4 " src={search} alt="" />
-                    </label>
-                </form>
-                <div className=' flex gap-1 w-[89px] h-6'>
-                    <img src={account} alt="" />
-                    <p className=' hidden lg:flex '>Account</p>
-                </div>
-
-
-                    
-
-                  
-                <Link to='/cart'>
-
-                        <div className=' flex gap-1 w-[89px] h-6'>
-                            <img src={cart} alt="" /> 
-                            <p className=' hidden lg:flex '>Cart</p>
-                        </div>  
- 
-                </Link>
-               </div> 
+          <div className=" flex flex-row items-center gap-10 ">
+            <form action="">
+              <label className="hidden input input-bordered rounded-3xl lg:flex items-center gap-2 bg-[f6f7f8] ">
+                <input type="text" className=" " placeholder="Search" />
+                <img className="" src={search} alt="" />
+              </label>
+            </form>
+            <div className="flex justify-between gap-5">
+              <Link to="/" className="flex gap-1">
+                <img src={account} alt="" />
+                <p className=" hidden lg:flex ">Account</p>
+              </Link>
+              <Link to="/cart" className=" flex gap-1 ">
+                <img src={cart} alt="" />
+                <p className=" hidden lg:flex ">Cart</p>
+              </Link>
+            </div>
+          </div>
         </div>
-        
-    </nav>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/home/TopContactHeader.jsx
+++ src/pages/home/TopContactHeader.jsx
@@ -1,43 +1,40 @@
-import React from 'react'
-import call from '../../assets/call.png'
-import arrowDown from '../../assets/arrowdownwhite.png'
+import React from "react";
+import call from "../../assets/call.png";
+import arrowDown from "../../assets/arrowdownwhite.png";
 
 const TopContactHeader = () => {
   return (
     <>
-        <div className='w-full h-14 bg-[#1E2019] text-white flex items-center '>
-            <div className='w-[90%] mx-auto flex items-center justify-between '>
-                {/* phone no */}
-                <div className=' flex'>
-                    <img src={call} alt="" />
-                    <p>+234903874698</p>
-                </div>
+      <div className="w-full h-12 bg-[#1E2019] text-white flex flex-col justify-center ">
+        <div className=" flex items-center justify-between mx-5 ">
+            <div className=" flex">
+                <img src={call} alt="" />
+                <p>+234903874698</p>
+            </div>
 
-                <div className=' hidden lg:block'>
-                    <p>Get 10% off on selected items | Shop Now</p>
-                </div>
+            <div className=" hidden lg:block">
+                <p>Get 10% off on selected items | Shop Now</p>
+            </div>
 
-                <div className='flex h-6 gap-6 '>
-                    <div className='flex mx-5 gap-3'>
-                        <p>Eng</p>
-                        <div className=' flex w-6 h-6 items-center'>
-                            <img src={arrowDown} alt="" />
-                        </div>
+            <div className="flex gap-6 ">
+                <div className="flex gap-3">
+                    <p>Eng</p>
+                    <div className=" flex items-center">
+                        <img src={arrowDown} alt="" />
                     </div>
-                    
-                    <div className='flex mx-5 gap-3'>
-                        <p>Location</p>
-                        <div className=' flex w-6 h-6 items-center'>
-                            <img src={arrowDown} alt="" />
-                        </div>
+                </div>
+
+                <div className="flex gap-3">
+                    <p>Location</p>
+                    <div className=" flex items-center">
+                        <img src={arrowDown} alt="" />
                     </div>
                 </div>
             </div>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default TopContactHeader;
-
-
