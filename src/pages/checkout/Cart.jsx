6e913121edbb9692de0d051daaf71747120193c5
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2025-01-15 14:11:22 +0100

    restructure, cart page

--- src/pages/checkout/Cart.jsx
+++ src/pages/checkout/Cart.jsx
@@ -1,70 +1,79 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import ButtonFilter from '../../components/ButtonFilter'
-import Card from '../../components/Card'
-import east from '../../assets/easts.png'
-import west from '../../assets/west.png'
-import topImage from '../../assets/5 1.png'
-import fav from '../../assets/favorite.png'
-import help from '../../assets/help.png'
-import plus from '../../assets/plus.png'
-import dash from '../../assets/dash.png'
-import basket from '../../assets/basket.png'
-import image1 from '../../assets/image1.png'
-import image2 from '../../assets/image2.png'
-import image3 from '../../assets/image3.png'
-import image4 from '../../assets/image4.png'
-import search from '../../assets/searchblack.png'
-import CardCarousel from '../../components/CardCarousel'
+import React from "react";
+import { Link } from "react-router-dom";
+import ButtonFilter from "../../components/ButtonFilter";
+import Card from "../../components/Card";
+import east from "../../assets/easts.png";
+import west from "../../assets/west.png";
+import topImage from "../../assets/5 1.png";
+import fav from "../../assets/favorite.png";
+import help from "../../assets/help.png";
+import plus from "../../assets/plus.png";
+import dash from "../../assets/dash.png";
+import basket from "../../assets/basket.png";
+import image1 from "../../assets/image1.png";
+import image2 from "../../assets/image2.png";
+import image3 from "../../assets/image3.png";
+import image4 from "../../assets/image4.png";
+import search from "../../assets/searchblack.png";
+import CardCarousel from "../../components/CardCarousel";
 
 const Cart = () => {
   return (
     <>
-      <div className="border w-full flex flex-col items-center bg-[#ffffff]">
-        <div className="w-[90%] flex flex-row md:flex justify-between border  ">
-          {/* grid grid-cols-1 lg:gap-0 border  */}
-          <div className=" flex flex-row md:flex justify-between lg:gap-0 lg:w-[50%] bg-[#FAFAFA] border border-xl">
-            {/* grid grid-cols-1 */}
-            <div className="">
-              {/* flex justify-between bg-yellow-600 grid grid-col-1  */}
+      <div className="border w-full flex flex-col items-center bg-[#fff] ">
+        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 p-2 gap-2">
+          {/* 1st card!!!!!! */}
+          <div className=" bg-[#FAFAFA] border rounded-xl p-4 space-y-2  ">
+            {/* <div className="">
               <img src={fav} alt="" />
-              <img src={topImage} alt="" className="w-[320px] h-[230px]" />
-            </div>
-            <div>
-              <div className="flex justify-between ">
-                <p className="font-[500px] text-[24px] py-4 pr-4">
-                  Mercedes Benz Cls
-                </p>
-                <h3 className="font-[600px] text-[32px]">$270.00</h3>
+            </div> */}
+            <div className=" grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+              {/* border rounded-xl border-gray-300 */}
+              <div className="flex flex-row items-start justify-end   bg-[#FAFAFA] p-2">
+                <img src={topImage} alt="" className="w-full h-full" />
+                <img src={fav} alt="" width={32} height={32} />
               </div>
-              <div className="">
-                <p>battery operated ride</p>
-                <p>Age 1-3 years</p>
-                <p>Colour black</p>
-              </div>
-              <div className="flex gap-20 px-6 pr-4">
-                <div className="flex w-[50%] justify-between ">
-                  <a href="">
-                    <img src={fav} alt="" />
-                  </a>
-                  <a href="">
-                    <img src={basket} alt="" />
-                  </a>
-                </div>
-                <div className="flex w-[50%] justify-between ">
-                  <a href="">
-                    <img src={dash} alt="" />
-                  </a>
-                  <p>1</p>
-                  <a href="">
-                    <img src={plus} alt="" />
-                  </a>
+              <div>
+                <div className=" space-y-8">
+                  <div className="flex justify-between ">
+                    <p className="font-[500px] text-[26px] ">
+                      Mercedes Benz Cls
+                    </p>
+                    <p className=" flex items-center font-[600px] text-[28px]">
+                      $270.00
+                    </p>
+                  </div>
+                  <div className=" space-y-1">
+                    <p>Battery Operated Ride</p>
+                    <p>Age: 1-3 year(s)</p>
+                    <p>Colour: Black</p>
+                  </div>
+                  <div className="grid grid-cols-2">
+                    <div className="grid grid-cols-2">
+                      <a href="/">
+                        <img src={fav} alt="" />
+                      </a>
+                      <a href="/">
+                        <img src={basket} alt="" />
+                      </a>
+                    </div>
+                    <div className="flex flex-between justify-end gap-2">
+                      <a href="/">
+                        <img src={dash} alt="" />
+                      </a>
+                      <p>1</p>
+                      <a href="/">
+                        <img src={plus} alt="" />
+                      </a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="lg:w-[50%] m-4 bg-[#FAFAFA] border rounded-xl ">
+          {/* 2nd card */}
+          <div className="bg-[#FAFAFA] border rounded-xl p-2">
             <h3 className="font-[600px] text-[32px]">Summary</h3>
             <div>
               <div className=" flex justify-between ">
@@ -105,24 +114,24 @@ const Cart = () => {
                 </label>
               </form>
             </div>
-
             <div className="flex justify-between my-4">
               <div className="flex gap-2 font-[500px] text-base ">
                 <p>Total </p>
               </div>
               <h3 className="font-[600px] text-2xl">$315.00</h3>
             </div>
-            <div className=" flex flex-col items-center justify-center">
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3 ">
               <button>
                 <Link to="/checkout">
-                  <div className="md:w-[250px] w-[143px] border border-[#840032] rounded-[100px] text-xs font-[400px] leading-[1.6] text-[#f8f8f8] bg-[#840032] text-center py-2">
+                  <div className="w-full border border-[#840032] rounded-[100px] text-xs font-[400px] leading-[1.6] text-[#f8f8f8] bg-[#840032] text-center py-2">
                     <p className="">Check out your cart</p>
                   </div>
                 </Link>
               </button>
               <button>
                 <a href="">
-                  <div className="md:w-[250px] w-[143px] border border-[#840032] rounded-[100px] text-xs font-[400px] leading-[1.6] text-[#840032] bg-[#f8f8f8] text-center py-2">
+                  <div className=" w-full border border-[#840032] rounded-[100px] text-xs font-[400px] leading-[1.6] text-[#840032] bg-[#f8f8f8] text-center py-2">
                     <p className="">Paypal</p>
                   </div>
                 </a>
@@ -145,68 +154,14 @@ const Cart = () => {
               </div>
               {/* arrows */}
               <div>
-              <CardCarousel />  
+                <CardCarousel />
               </div>
-              
-              {/* <div className="">
-                <ul className="flex gap-2 items-center h-full   ">
-                  <li>
-                    <a href="">
-                      <img src={east} alt="" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="">
-                      <img src={west} alt="" />
-                    </a>
-                  </li>
-                </ul>
-              </div> */}
             </div>
-
-            {/* cards */}
-            {/* <div className="md:flex justify-between gap-3 grid grid-cols-1 lg:gap-3 w-full items-center ">
-              {arr.map((card, i) => {
-                return <Card key={i} {...card} />;
-              })}
-            </div> */}
           </div>
         </div>
       </div>
     </>
   );
-}
+};
 
-const arr = [
-	{
-		image: image1,
-		text: "Thar 888 jeep, battery operated jeep",
-    priceblack: "$200.00",
-    pricered: "$260.00",
-		rate: "7/10 Sales",
-	},
-	{
-		image: image2,
-		text: "Mercedes Benz 12V, battery operated ride",
-    priceblack: "$270.00",
-    pricered: "$300.00",
-		rate: "7/10 Sales",
-	},
-	{
-		image: image3,
-		text: "super bike, battery operated",
-    priceblack: "$90.00",
-    pricered: "$110.00",
-		rate: "7/10 Sales"
-	},
-	{
-		image: image4,
-		text: "Rolls Royce, battery operated ride",
-    priceblack: "$300.00",
-    pricered: "$320.00",
-		rate: "7/10 Sales",
-	},
-
-];
-
-export default Cart
+export default Cart;
